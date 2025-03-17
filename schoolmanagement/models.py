ca9328e1b9a9fc036a20738b3892442b600f8057
Jitendra7529 <joshizeet@gmail.com>
2025-03-17 15:48:51 +0545

    Updated

--- schoolmanagement/models.py
+++ schoolmanagement/models.py
@@ -3,21 +3,21 @@ from django.db import models
 # Create your models here.
 
 class Teacher(models.Model):
-    firstname = models.CharField(max_length = 10)
-    lastname = models.CharField(max_length = 10)
+    firstname = models.CharField(max_length = 10, blank=False)
+    lastname = models.CharField(max_length = 10, blank=False)
 
     def __str__(self):
         return f"{self.firstname} {self.lastname}"
     
 class Student(models.Model):
-    firstname = models.CharField(max_length = 8)
-    lastname = models.CharField(max_length = 9 )
+    firstname = models.CharField(max_length = 8, blank=False)
+    lastname = models.CharField(max_length = 9, blank=False )
 
     def __str__(self):
         return f"{self.firstname} {self.lastname}"
 
 class Subject(models.Model):
-    name = models.CharField(max_length = 20)
+    name = models.CharField(max_length = 20, blank=False)
 
     def __str__(self):
         return self.name
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Mavorion\firstproject\venv
+set VIRTUAL_ENV=D:\Mavorion_systems\practice_projects\firstproject\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
